7e0aef3e9898586842e71bf9331889b7bf12f95c
Ghosterbeef <Ghosterbeef@github.com>
2021-04-25 22:41:04 +0300

    Серверные настройки обновлены (Тест)

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+// const express = require('express')
+// const serveStatic = require('serve-static')
+// const history = require('connect-history-api-fallback')
+// const enforce = require('express-sslify')
+//
+// const app = express()
+//
+// app.use(enforce.HTTPS({trustProtoHeader: true}))
+// app.use(serveStatic(__dirname + '/dist'))
+// app.use(history({verbose: true}))
+//
+// app.listen(process.env.PORT || 5000)
+
 const express = require('express')
-const serveStatic = require('serve-static')
+const path = require('path')
 const history = require('connect-history-api-fallback')
-const enforce = require('express-sslify')
+// ^ middleware to redirect all URLs to index.html
 
 const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname))
 
-app.use(enforce.HTTPS({trustProtoHeader: true}))
-app.use(serveStatic(__dirname + '/dist'))
-app.use(history({verbose: true}))
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
 
-app.listen(process.env.PORT || 5000)
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/index.html'))
+})
