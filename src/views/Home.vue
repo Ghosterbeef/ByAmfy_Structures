44d90a294a41542429350b852163c59f3db5f29c
Ghosterbeef <Ghosterbeef@github.com>
2021-05-02 20:38:02 +0300

    3д карточки на главной странице

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,24 @@
                 <router-link to="/NotFound">К котику!</router-link>
             </div>
             <nav>
-                <router-link to="/avl_tree">
-                    <h2>AVL дерево</h2>
-                </router-link>
-                <router-link to="/b_tree">
-                    <h2>B дерево</h2>
-                </router-link>
-                <router-link to="/hash_table">
-                    <h2>Хеш-таблица</h2>
-                </router-link>
+                <div ref="card_container1" class="card_container" @mousemove="onMouseMove" @mouseenter="onMouseEnter"
+                     @mouseleave="onMouseLeave">
+                    <router-link to="/avl_tree">
+                        <h2>AVL дерево</h2>
+                    </router-link>
+                </div>
+                <div ref="card_container2" class="card_container" @mousemove="onMouseMove" @mouseenter="onMouseEnter"
+                     @mouseleave="onMouseLeave">
+                    <router-link to="/b_tree">
+                        <h2>B дерево</h2>
+                    </router-link>
+                </div>
+                <div ref="card_container3" class="card_container" @mousemove="onMouseMove" @mouseenter="onMouseEnter"
+                     @mouseleave="onMouseLeave">
+                    <router-link to="/hash_table">
+                        <h2>Хеш-таблица</h2>
+                    </router-link>
+                </div>
             </nav>
         </div>
     </div>
@@ -22,18 +31,59 @@
 <script>
     export default {
         name: "Home",
+        data() {
+            return {}
+        },
+        methods: {
+            onMouseMove: function (e) {
+                let target
+                if (e.target.classList.contains("card_container"))
+                    target = e.target.childNodes[0]
+                else if (e.target.parentNode.classList.contains("card_container"))
+                    target = e.target
+                else
+                    target = e.target.parentNode
+                const xAxis = (300 / 2 - e.offsetX) / 10
+                const yAxis = (300 / 2 - e.offsetY) / 10
+                target.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+            },
+            onMouseEnter: function (e) {
+                let target
+                if (e.target.classList.contains("card_container"))
+                    target = e.target.childNodes[0]
+                else if (e.target.parentNode.classList.contains("card_container"))
+                    target = e.target
+                else
+                    target = e.target.parentNode
+                target.style.transition = 'none'
+                target.childNodes[0].style.transform = 'translateZ(100px)'
+            },
+            onMouseLeave: function (e) {
+                let target
+                if (e.target.classList.contains("card_container"))
+                    target = e.target.childNodes[0]
+                else if (e.target.parentNode.classList.contains("card_container"))
+                    target = e.target
+                else
+                    target = e.target.parentNode
+                target.style.transition = '0.5s'
+                target.style.transform = `rotateY(0deg) rotateX(0deg)`
+                target.childNodes[0].style.transform = 'translateZ(0px)'
+            }
+        }
     }
 </script>
 
 <style scoped>
-    .home-section{
+    .home-section {
         display: flex;
         justify-content: center;
         align-items: center;
         min-height: 50vh;
+        perspective: 1000px;
     }
 
-    .rofl{
+    .rofl {
         position: absolute;
         opacity: 0;
         left: 50px;
@@ -41,11 +91,11 @@
         transition: 0.1s;
     }
 
-    .rofl:hover{
+    .rofl:hover {
         opacity: 1;
     }
 
-    .rofl a{
+    .rofl a {
         padding: 20px;
         background-color: #69BEEB;
         border-radius: 20px;
@@ -61,21 +111,36 @@
         justify-content: space-evenly;
     }
 
-    nav a{
+    .card_container {
+        transform-style: preserve-3d;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 300px;
+        height: 300px;
+        margin: 20px;
+        overflow: visible;
+    }
+
+    nav a {
+        transform-style: preserve-3d;
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        margin: 20px;
         background-color: gray;
         width: 300px;
         height: 300px;
         color: white;
-        transition: 0.25s;
         box-shadow: 0 0 25px 7px darkgrey;
     }
 
-    nav a:hover{
-        transform: translateY(-5px);
+    nav a h2 {
+        transition: 0.2s;
+        text-align: center;
+        line-height: 300px;
+        width: 300px;
+        height: 300px;
     }
+
 </style>
